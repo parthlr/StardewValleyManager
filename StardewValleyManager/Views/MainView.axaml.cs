e46cd456857d4f376ffc257a4b548307e416e98e
parthlr <parthlr321@gmail.com>
2024-08-24 22:24:38 -0400

    Initial commit

---
+++ StardewValleyManager/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace StardewValleyManager.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
